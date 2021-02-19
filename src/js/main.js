9c9850cd7f806bf5b083efbd629c548bb7028263
kotosvin <erhan.denis@list.ru>
2021-02-20 00:10:56 +0500

    #2

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
-function koloda() {
-  let koloda_array = [];
+function createDeck() {
+  let deck = [];
   let worth = [ '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' ];
   let suit = [ 'c', 'd', 's', 'h'];
   let value = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 ];
   for (let i = 0; i < suit.length; i++) {
     for (let j = 0; j < worth.length; j++) {
-      koloda_array.push([ worth[j], suit[i], value[j] ]);
+      deck.push([ worth[j], suit[i], value[j] ]);
     }
   }
-  return koloda_array;
+  return deck;
+}
+
+function randomCard(deck) {
+  let index = Math.floor(Math.random() * deck.length);
+  return deck.splice(index, 1)[0];
 }
